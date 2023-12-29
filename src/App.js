33c35e60f7790a577d7c2eb007228d8c85bd80e5
Payalsasmal10 <sasmalpayal@gmail.com>
2023-12-29 21:05:11 +0530

    creating bucket list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState} from "react";
 
 function App() {
+  const bucket1 = ["hi", "hello", "testing"];
+  const bucket2 = ["hi there", "hello there", "testing there"];
+  const [bucketList1, setBucketList1] = useState(bucket1);
+  const [bucketList2, setBucketList2] = useState(bucket2);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="">
+      <section>
+        <h3>BucketList1</h3>
+        <div>
+        {bucket1.map((item) => {
+          return(
+          <ul key={item}>
+            <li>{item}</li>
+          </ul>
+          );
+        })}
+        
+        </div>
+      </section>
+      <section>
+        <h3>BucketList2</h3>
+        <div>
+        {bucket2.map((item) => {
+          return(
+          <ul key={item}>
+            <li>{item}</li>
+          </ul>
+          );
+        })}
+        
+        </div>
+      </section>
+    </main>
   );
 }
 
