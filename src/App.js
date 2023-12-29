96ec23fcb8abe2c4588a549738bc899c9a8f2c82
Payalsasmal10 <sasmalpayal@gmail.com>
2023-12-29 23:06:17 +0530

    creating bucket list

--- src/App.js
+++ src/App.js
@@ -4,19 +4,64 @@ import { useState} from "react";
 function App() {
   const bucket1 = ["hi", "hello", "testing"];
   const bucket2 = ["hi there", "hello there", "testing there"];
-  const [bucketList1, setBucketList1] = useState(bucket1);
-  const [bucketList2, setBucketList2] = useState(bucket2);
+  const [bucketList1, setBucketList1] = useState(bucket1.map((item) => ({value: item, checked: false})));
+  const [bucketList2, setBucketList2] = useState(bucket2.map((item) => ({value:item, checked: false})));
 
+  const bucketList1Handler = (index) => {
+    const newBucketList = [...bucketList1];
+    newBucketList[index].checked = !newBucketList[index].checked;
+    console.log("newBuckerList", newBucketList);
+    newBucketList.forEach((item, idx) => {
+      if(index !==idx){
+        item.checked = false;
+      }
+    })
+    setBucketList1(newBucketList);
+  };
+
+  const bucketList2Handler = (index) => {
+    const newBucketList = [...bucketList2];
+    newBucketList[index].checked = !newBucketList[index].checked;
+    newBucketList.forEach((item, idx) => {
+      if(index !==idx){
+        item.checked = false;
+      }
+    })
+    setBucketList2(newBucketList);
+  };
+
+  const moveHandler = (direction) => {
+    const checkedItemsBucketList1 = bucketList1.filter((item) => item.checked);
+    const uncheckedItemsBucketList1 = bucketList1.filter((item) => !item.checked);
+    
+    const checkedItemsBucketList2 = bucketList2.filter((item) => item.checked);
+    const uncheckedItemsBucketList2 = bucketList2.filter((item) => !item.checked);
+
+    let newBucketList;
+    if(direction === "Down"){
+      newBucketList = [...bucketList2, ...checkedItemsBucketList1];
+      setBucketList2(newBucketList);
+      setBucketList1([...uncheckedItemsBucketList1]);
+    }else{
+      newBucketList = [...bucketList1, ...checkedItemsBucketList2];
+      setBucketList1(newBucketList);
+      setBucketList2(uncheckedItemsBucketList2);
+    }
+  }
+
+  console.log(bucketList1);
 
   return (
     <main className="">
+        <button onClick={() => moveHandler("Up")}>Up</button>
+        <button onClick={() => moveHandler('Down')}>Down</button>
       <section>
         <h3>BucketList1</h3>
         <div>
-        {bucket1.map((item) => {
+        {bucketList1.map((item, index) => {
           return(
-          <ul key={item}>
-            <li>{item}</li>
+          <ul key={item.value}>
+            <input type='checkbox' onChange={() => bucketList1Handler(index)}/><span>{item.value}</span>
           </ul>
           );
         })}
@@ -26,16 +71,17 @@ function App() {
       <section>
         <h3>BucketList2</h3>
         <div>
-        {bucket2.map((item) => {
+        {bucketList2.map((item, index) => {
           return(
-          <ul key={item}>
-            <li>{item}</li>
+          <ul key={item.value}>
+            <input type='checkbox' onChange={() => bucketList2Handler(index)}/><span>{item.value}</span>
           </ul>
           );
         })}
         
         </div>
       </section>
+
     </main>
   );
 }
